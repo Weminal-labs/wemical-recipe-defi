a8221ca21257d3ecbd6f5c8901d9d4bd547f1acf
Le Khac Thanh Tung <lekhacthanhtung.it@gmail.com>
2024-04-15 18:17:12 +0700

    bugfix: hardcode address for swap

--- src/Aftermath.tsx
+++ src/Aftermath.tsx
@@ -1,4 +1,7 @@
-import { useSignAndExecuteTransactionBlock } from "@mysten/dapp-kit";
+import {
+  useCurrentAccount,
+  useSignAndExecuteTransactionBlock,
+} from "@mysten/dapp-kit";
 import {
   SuiClient,
   SuiHTTPTransport,
@@ -15,10 +18,14 @@ import {
 import axios from "axios";
 
 import React, { useEffect, useState } from "react";
-import { createTxbStake, createTxbSwap, createTxbSwapOposite } from "./api/aftermath";
+import {
+  createTxbStake,
+  createTxbSwap,
+  createTxbSwapOposite,
+} from "./api/aftermath";
 
 export default function index() {
-
+  const currentAccount = useCurrentAccount();
   const { mutate: signAndExecute } = useSignAndExecuteTransactionBlock();
 
   const handleStake = async () => {
@@ -44,7 +51,10 @@ export default function index() {
   };
 
   const handleSwap = async () => {
-    const txb = await createTxbSwap(BigInt(1_000_000_000));
+    const txb = await createTxbSwap(
+      BigInt(1_000_000_000),
+      currentAccount?.address,
+    );
 
     signAndExecute(
       {
@@ -66,7 +76,10 @@ export default function index() {
   };
 
   const handleSwapOposite = async () => {
-    const txb = await createTxbSwapOposite(BigInt(1_000_000_000));
+    const txb = await createTxbSwapOposite(
+      BigInt(1_000_000_000),
+      currentAccount?.address,
+    );
 
     signAndExecute(
       {
@@ -90,9 +103,27 @@ export default function index() {
   return (
     <>
       <div>Aftermath</div>
-      <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleStake}>Stake</button>
-      <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleSwap}>Swap</button>
-      <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleSwapOposite}>Swap Oposite</button>
+      <button
+        type="button"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleStake}
+      >
+        Stake
+      </button>
+      <button
+        type="button"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleSwap}
+      >
+        Swap
+      </button>
+      <button
+        type="button"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleSwapOposite}
+      >
+        Swap Oposite
+      </button>
     </>
   );
 }
